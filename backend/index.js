f32f18f034bfbf38bd85341e3576dc94997deca5
Parth Sharma <154320618+parthsh17@users.noreply.github.com>
2025-08-23 10:13:56 +0530

    made changes

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,22 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/transactions', transactionRoutes);
+
+const rootDir = path.resolve(__dirname, '..'); 
+const frontendPath = path.join(rootDir, 'frontend', 'dist'); 
+
+
+app.use(express.static(frontendPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
